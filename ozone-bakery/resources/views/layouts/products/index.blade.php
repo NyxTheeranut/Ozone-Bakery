8a8b79ba8a1a95c7be9ea03cf520638c8405ed39
Nyx <theeranut.s@ku.th>
2023-10-28 16:12:25 +0700

    profile page

--- ozone-bakery/resources/views/layouts/products/index.blade.php
+++ ozone-bakery/resources/views/layouts/products/index.blade.php
@@ -1,38 +1,40 @@
 @extends('layouts.main')
 
 @section('content')
+    <section class="bg-neutral-100 flex flex-col text-4xl pt-8 pb-20">
+        <div class="flex flex-col">
+            <div class="flex justify-content-between">
+                <div class="flex flex-col items-stretch w-6/12 max-md:w-full">
+                    <div id="scrollTarget"
+                        class="relative shrink-0 box-border h-auto text-3xl grow-0 w-auto ml-20 mt-5 mb-2.5 pl-10">
+                        All Product
+                    </div>
+                </div>
+                <div class="flex flex-col items-stretch w-6/12 ml-5 max-md:w-full">
+                    @auth
+                        @if (auth()->user()->is_admin == 1)
+                            <a href="#"
+                                class="shrink-0 box-border bg-stone-500 text-white no-underline text-base mx-auto px-5 py-2.5 rounded-3xl hover:bg-stone-600 ml-auto mt-5 mr-20 bg-stone-500">
+                                Add product
+                            </a>
+                        @endif
+                    @endauth
+                </div>
+            </div>
+            <div
+                class="flex flex-wrap max-md:flex-col max-md:items-stretch max-md:gap- ml-20 mr-20 pl-20 pr-20 mt-5 mx-auto">
+                @foreach ($products as $product)
+                    <div class="flex flex-col items-stretch w-3/12 max-md:w-full mb-5">
+                        <a href="/products/{{ $product->id }}"
+                            class="flex flex-col relative shrink-0 box-border h-auto shadow-[3px_-2px_26px_-20px_rgba(0,0,0,1)] w-[300px] self-center mt-5 pb-8 rounded-3xl border-[3px] border-solid border-stone-300">
+                            <img src="{{ asset($product->image_path) }}" alt="{{ $product->name }} "
+                                class="aspect-[1.46] object-cover object-center w-[250px] h-[250px] mx-auto my-5 rounded-2xl border-0 border-solid">
 
-<section class="bg-neutral-100 flex flex-col text-4xl pt-8 pb-20">
-  <div class="flex flex-col">
-    <div class="flex justify-content-between">
-        <div class="flex flex-col items-stretch w-6/12 max-md:w-full">
-          <div id="scrollTarget" class="relative shrink-0 box-border h-auto text-3xl grow-0 w-auto ml-20 mt-5 mb-2.5 pl-10">
-            All Product
-          </div>
-        </div>
-        <div class="flex flex-col items-stretch w-6/12 ml-5 max-md:w-full">
-          @auth
-              @if(auth()->user()->is_admin == 1)
-                  <a href="#" class="shrink-0 box-border bg-stone-500 text-white no-underline text-base mx-auto px-5 py-2.5 rounded-3xl hover:bg-stone-600 ml-auto mt-5 mr-20 bg-stone-500">
-                      Add product
-                  </a>
-              @endif
-          @endauth
-      </div>
-</div>
-    <div class="flex flex-wrap max-md:flex-col max-md:items-stretch max-md:gap- ml-20 mr-20 pl-20 pr-20 mt-5 mx-auto">
-        @foreach ($products as $product)
-      <div class="flex flex-col items-stretch w-3/12 max-md:w-full mb-5">
-          <a href="/products/{{  $product->id  }}" class="flex flex-col relative shrink-0 box-border h-auto shadow-[3px_-2px_26px_-20px_rgba(0,0,0,1)] w-[300px] self-center mt-5 pb-8 rounded-3xl border-[3px] border-solid border-stone-300">
-              <img src="{{ asset($product->image_path) }}" alt="{{ $product->name }} " class="aspect-[1.46] object-cover object-center w-[250px] h-[250px] mx-auto my-5 rounded-2xl border-0 border-solid">
-
-              <h3 class="text-2xl mx-auto">{{  $product->name  }}</h3>
-              <p class="text-xl mx-auto my-2.5">Price: {{  $product->price  }} Baht</p>
-          </a>
-      </div>
-      @endforeach
-    </div>
-  </section>
-
+                            <h3 class="text-2xl mx-auto">{{ $product->name }}</h3>
+                            <p class="text-xl mx-auto my-2.5">Price: {{ $product->price }} Baht</p>
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+    </section>
 @endsection
-
--- ozone-bakery/resources/views/profile/index.blade.php
+++ ozone-bakery/resources/views/profile/index.blade.php
@@ -1,132 +1,119 @@
 @extends('layouts.main')
 
 @section('content')
-<section class="w-100 px-4 py-5">
-      <div class="row d-flex justify-content-center snipcss0-0-0-1">
-        <div class="col col-lg-7 mb-4 mb-lg-0 snipcss0-1-1-2">
-          <div class="card mt-20">
-              <div class="col-md-8 snipcss0-4-4-10">
-                <div class="card-body p-4 snipcss0-5-10-11">
-                  <h6 class="text-2xl font-bold">
-                    Profile
-                  </h6>
-                  <hr class="mt-0 mb-2 snipcss0-6-11-13">
-                  <div class="row pt-1 snipcss0-6-11-14">
-                    <div class="col-6 mb-3 snipcss0-7-14-15">
-                      <h6 class="font-bold">
-                        Firstname
-                      </h6>
-                      <p class="text-muted snipcss0-8-15-17">
-                        {{ $user->name }}
-                      </p>
+    <section class="w-100 px-4 py-5">
+        <div class="row d-flex justify-content-center snipcss0-0-0-1">
+            <div class="col col-lg-7 mb-4 mb-lg-0 snipcss0-1-1-2">
+                <div class="card mt-20">
+                    <div class="col-md-8 snipcss0-4-4-10">
+                        <div class="card-body p-4 snipcss0-5-10-11">
+                            <h6 class="text-2xl font-bold">
+                                Profile
+                            </h6>
+                            <hr class="mt-0 mb-2 snipcss0-6-11-13">
+                            <div class="row pt-1 snipcss0-6-11-14">
+                                <div class="col-6 mb-3 snipcss0-7-14-15">
+                                    <h6 class="font-bold">
+                                        Firstname
+                                    </h6>
+                                    <p class="text-muted snipcss0-8-15-17">
+                                        {{ $user->name }}
+                                    </p>
+                                </div>
+                                <div class="col-6 mb-3 snipcss0-7-14-18">
+                                    <h6 class="font-bold">
+                                        Lastname
+                                    </h6>
+                                    <p class="text-muted snipcss0-8-18-20">
+                                        {{ $user->lastname }}
+                                    </p>
+                                </div>
+                            </div>
+                            <h6 class="text-xl font-bold">
+                                Personal
+                            </h6>
+                            <hr class="mt-0 mb-2 snipcss0-6-11-22">
+                            <div class="row pt-1 snipcss0-6-11-23">
+                                <div class="col-6 mb-3 snipcss0-7-14-18">
+                                    <h6 class="font-bold">
+                                        Phone
+                                    </h6>
+                                    <p class="text-muted snipcss0-8-18-20">
+                                        {{ $user->tel }}
+                                    </p>
+                                </div>
+                                <div class="col-6 mb-3 snipcss0-7-23-24">
+                                    <h6 class="font-bold">
+                                        Email
+                                    </h6>
+                                    <p class="text-muted snipcss0-8-24-26">
+                                        {{ $user->email }}
+                                    </p>
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-6 mb-3 snipcss0-7-14-18">
-                      <h6 class="font-bold">
-                        Lastname
-                      </h6>
-                      <p class="text-muted snipcss0-8-18-20">
-                          {{ $user->lastname }}
-                      </p>
-                    </div>
-                  </div>
-                  <h6 class="text-xl font-bold">
-                    Personal
-                  </h6>
-                  <hr class="mt-0 mb-2 snipcss0-6-11-22">
-                  <div class="row pt-1 snipcss0-6-11-23">
-                    <div class="col-6 mb-3 snipcss0-7-14-18">
-                        <h6 class="font-bold">
-                          Phone
-                        </h6>
-                        <p class="text-muted snipcss0-8-18-20">
-                            {{ $user->tel }}
-                        </p>
-                      </div>
-                    <div class="col-6 mb-3 snipcss0-7-23-24">
-                      <h6 class="font-bold">
-                        Email
-                      </h6>
-                      <p class="text-muted snipcss0-8-24-26">
-                          {{ $user->email }}
-                      </p>
-                    </div>
-                    <div class="col-6 mb-3 snipcss0-7-23-24">
-                    @if (Auth::check())
-                        @if (Auth::user()->role === 'MEMBER')
-                          <button id="certificatePopupButton"class="snipcss0-8-24-25 btn">
-                            Certificate
-                          </button>
-                        @endif
-                    @endif
-                    </div>
-                  </div>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
+        </div>
     </section>
 @endsection
 
 <style>
+    * {
+        box-sizing: border-box;
+    }
 
+    .row>* {
+        flex-shrink: 0;
+        width: 100%;
+        max-width: 100%;
+        padding-right: calc(var(--mdb-gutter-x)*0.5);
+        padding-left: calc(var(--mdb-gutter-x)*0.5);
+        margin-top: var(--mdb-gutter-y);
+    }
 
+    .row {
+        --mdb-gutter-x: 1.5rem;
+        --mdb-gutter-y: 0;
+        display: flex;
+        flex-wrap: wrap;
+        margin-top: calc(var(--mdb-gutter-y)*-1);
+        margin-right: calc(var(--mdb-gutter-x)*-0.5);
+        margin-left: calc(var(--mdb-gutter-x)*-0.5);
+    }
 
-* {
-    box-sizing: border-box;
-}
+    .justify-content-center {
+        justify-content: center !important;
+    }
 
-.row > *  {
-    flex-shrink: 0;
-    width: 100%;
-    max-width: 100%;
-    padding-right: calc(var(--mdb-gutter-x)*0.5);
-    padding-left: calc(var(--mdb-gutter-x)*0.5);
-    margin-top: var(--mdb-gutter-y);
-}
+    @media (min-width: 200px) {
+        .col-lg-7 {
+            width: 35%;
+        }
+    }
 
-.row {
-    --mdb-gutter-x: 1.5rem;
-    --mdb-gutter-y: 0;
-    display: flex;
-    flex-wrap: wrap;
-    margin-top: calc(var(--mdb-gutter-y)*-1);
-    margin-right: calc(var(--mdb-gutter-x)*-0.5);
-    margin-left: calc(var(--mdb-gutter-x)*-0.5);
-}
+    .mb-4 {
+        margin-bottom: 1.5rem !important;
+    }
 
-.justify-content-center {
-    justify-content: center!important;
-}
+    /*noinspection ALL*/
+    .card {
+        --mdb-card-border-radius: 0.5rem;
+        --mdb-card-box-shadow: 0 2px 15px -3px rgba(0, 0, 0, 0.07), 0 10px 20px -2px rgba(0, 0, 0, 0.04);
+        --mdb-card-bg: #fff;
 
-@media (min-width: 200px){
-  .col-lg-7 {
-    width: 35%;
-  }
-}
+        height: var(--mdb-card-height);
+        background-color: var(--mdb-card-bg);
+        border: var(--mdb-card-border-width) solid var(--mdb-card-border-color);
+        border-radius: var(--mdb-card-border-radius);
+        box-shadow: var(--mdb-card-box-shadow);
+    }
 
-.mb-4 {
-    margin-bottom: 1.5rem!important;
-}
-
-/*noinspection ALL*/
-.card {
-    --mdb-card-border-radius: 0.5rem;
-    --mdb-card-box-shadow: 0 2px 15px -3px rgba(0,0,0,0.07),0 10px 20px -2px rgba(0,0,0,0.04);
-    --mdb-card-bg: #fff;
-
-    height: var(--mdb-card-height);
-    background-color: var(--mdb-card-bg);
-    border: var(--mdb-card-border-width) solid var(--mdb-card-border-color);
-    border-radius: var(--mdb-card-border-radius);
-    box-shadow: var(--mdb-card-box-shadow);
-}
-
-.col-6 {
-    flex: 0 0 auto;
-    width: 50%;
-}
-
-    
+    .col-6 {
+        flex: 0 0 auto;
+        width: 50%;
+    }
 </style>
