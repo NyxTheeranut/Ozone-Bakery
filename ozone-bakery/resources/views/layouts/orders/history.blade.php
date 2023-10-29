c88ccf262d5d95fcebeaa48524d017c91724d46f
Nyx <theeranut.s@ku.th>
2023-10-30 00:17:08 +0700

    auth check order history

--- ozone-bakery/resources/views/layouts/orders/history.blade.php
+++ ozone-bakery/resources/views/layouts/orders/history.blade.php
@@ -22,61 +22,60 @@
 
         <div id="order-p" class="order-content">
             @foreach ($orders as $order)
-                <a href="/history/{{ $order->id }}">
-                    <div class="bg-stone-100 rounded-xl shadow-lg mt-7 p-4 sm:p-7 hover:bg-stone-200">
+                @if (Auth::check() && $order->user_id == Auth::user()->id)
+                    <a href="/history/{{ $order->id }}">
+                        <div class="bg-stone-100 rounded-xl shadow-lg mt-7 p-4 sm:p-7 hover:bg-stone-200">
 
-                        <h1 class="mb-3 text-2xl font-bold">
-                            Order #{{ $order->id }}
-                        </h1>
+                            <h1 class="mb-3 text-2xl font-bold">
+                                Order #{{ $order->id }}
+                            </h1>
 
-                        <hr class="mt-0 mb-4" style="border-color:#c4b7a6; border-width: 2px;">
+                            <hr class="mt-0 mb-4" style="border-color:#c4b7a6; border-width: 2px;">
 
-                        <p class="ml-3 mb-2 text-xl font-semibold">
-                            Date: {{ $order->created_at }}
-                        </p>
+                            <p class="ml-3 mb-2 text-xl font-semibold">
+                                Date: {{ $order->created_at }}
+                            </p>
 
-                        <p class="ml-3 mb-2 text-xl font-semibold">
-                            Payment Status: {{ $order->payment_status }}
-                        </p>
+                            <p class="ml-3 mb-2 text-xl font-semibold">
+                                Payment Status: {{ $order->payment_status }}
+                            </p>
 
-                        <p class="ml-3 text-xl font-semibold">
-                            Total: {{ $order->amount }} Baht
-                        </p>
-                    </div>
-                </a>
+                            <p class="ml-3 text-xl font-semibold">
+                                Total: {{ $order->amount }} Baht
+                            </p>
+                        </div>
+                    </a>
+                @endif
             @endforeach
         </div>
 
         <div id="made-to-order-p" class="order-content" style="display: none;">
             @foreach ($madeToOrderData as $madeToOrder)
-                <a href="/history/{{ $madeToOrder->id }}">
-                    <div class="bg-stone-100 rounded-xl shadow-lg mt-7 p-4 sm:p-7 hover:bg-stone-200">
-
-                        <h1 class="mb-3 text-2xl font-bold">
-                            Order #{{ $madeToOrder->id }}
-                        </h1>
-
-                        <hr class="mt-0 mb-4" style="border-color:#c4b7a6; border-width: 2px;">
-
-                        <p class="ml-3 mb-2 text-xl font-semibold">
-                            Date: {{ $madeToOrder->created_at }}
-                        </p>
-
-                        <p class="ml-3 mb-2 text-xl font-semibold">
-                            Status: {{ $madeToOrder->order_status }}
-                        </p>
-
-                        <p class="ml-3 mb-2 text-xl font-semibold">
-                            Total: {{ $madeToOrder->price }} Baht
-                        </p>
-
-                        <p class="ml-3 text-xl font-semibold">
-                            Pick-up Date: {{ $madeToOrder->pickup_date }}
-                        </p>
-                    </div>
-                </a>
+                @if (Auth::check() && $madeToOrder->user_id == Auth::user()->id)
+                    <a href="/history/{{ $madeToOrder->id }}">
+                        <div class="bg-stone-100 rounded-xl shadow-lg mt-7 p-4 sm:p-7 hover:bg-stone-200">
+                            <h1 class="mb-3 text-2xl font-bold">
+                                Order #{{ $madeToOrder->id }}
+                            </h1>
+                            <hr class="mt-0 mb-4" style="border-color:#c4b7a6; border-width: 2px;">
+                            <p class="ml-3 mb-2 text-xl font-semibold">
+                                Date: {{ $madeToOrder->created_at }}
+                            </p>
+                            <p class="ml-3 mb-2 text-xl font-semibold">
+                                Status: {{ $madeToOrder->order_status }}
+                            </p>
+                            <p class="ml-3 mb-2 text-xl font-semibold">
+                                Total: {{ $madeToOrder->price }} Baht
+                            </p>
+                            <p class="ml-3 text-xl font-semibold">
+                                Pick-up Date: {{ $madeToOrder->pickup_date }}
+                            </p>
+                        </div>
+                    </a>
+                @endif
             @endforeach
         </div>
+
 
     </div>
 
