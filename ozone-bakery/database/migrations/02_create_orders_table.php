b4ab0513ef6a5e7ada7e2d1cb016ac78020e618a
dujrawee <dujrawee.l@ku.th>
2023-10-31 22:06:57 +0700

    fix order history

--- ozone-bakery/database/migrations/02_create_orders_table.php
+++ ozone-bakery/database/migrations/02_create_orders_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
 
             $table->foreignId('user_id')->constrained('users');
-            $table->unsignedInteger('amount');
             $table->enum('payment_status', ['Pending', 'Completed', 'Failed'])->default('Pending');
 
             $table->timestamps();
