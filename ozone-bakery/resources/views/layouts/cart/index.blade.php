662e1f6cbad4e74c0871d4cee04c17de45ad671f
Nyx <theeranut.s@ku.th>
2023-11-01 01:26:17 +0700

    if no item

--- ozone-bakery/resources/views/layouts/cart/index.blade.php
+++ ozone-bakery/resources/views/layouts/cart/index.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.main')
 
 @section('content')
-    <form method="POST" action="{{ route('cart.update') }}" id="cart-form">
+<h1>Shopping Cart</h1>
+<form method="POST" action="{{ route('cart.update') }}" id="cart-form">
         @csrf
         @method('PUT')
 
@@ -9,38 +10,68 @@
             $totalPrice = 0;
         @endphp
         <div>
-            @foreach ($carts as $item)
-                <div class="max-w-screen-lg px-4 py-1 sm:px-2 lg:px-2 lg:py-1 mx-auto">
-                    <div class="bg-white rounded-xl shadow-lg mt-7 p-4 sm:p-7 ">
-                        <div class="mb-3 flex flex-wrap">
-                            <section class="flex flex-col items-center justify-center w-[25%] max-md:w-full">
-                                <div class="flex flex-wrap items-center">
-                                    <img src="{{ asset($item->product->image_path) }}" class="h-40 w-40 rounded-3xl">
-                                </div>
-                            </section>
-
-                            <section class="flex flex-col items-stretch w-[40%] ml-5 max-md:w-full">
-                                <h2 class="mt-2 text-3xl font-bold text-gray-800 text-black text-left">
-                                    {{ $item->product->name }}
+            @if ($carts->isEmpty())
+                <!-- No products message -->
+                <div class="flex flex-col">
+                    <div class="-m-1.5 overflow-x-auto">
+                        <div class="p-1.5 min-w-full inline-block align-middle">
+                            <!-- Body -->
+                            <div class="max-w-sm w-full min-h-[400px] flex flex-col justify-center mx-auto px-6 py-4">
+                                <h2 class="mt-5 font-semibold text-gray-800 dark:text-black text-center">
+                                    No products?
                                 </h2>
-
-                                <h1 class="text-2xl font-semibold mt-auto mb-3">
-                                    Quantities: <input type="number" class="quantity-input text-center rounded-xl"
-                                        name="cart_items[{{ $item->id }}][amount]" value="{{ $item->amount }}"
-                                        min="0" max="100"> pieces
-                                </h1>
-                            </section>
-                            <section class="flex flex-col items-stretch w-[20%] ml-auto mt-auto max-md:w-full">
-                                <span class="item-price text-2xl font-semibold mt-auto mb-3">Price:
-                                    {{ $item->product->price }} Baht</span>
-                            </section>
+                                <p class="mt-2 text-sm text-gray-600 dark:text-gray-400 text-center">
+                                    Find yourself a nice bakery below
+                                </p>
+                                <div class="mt-5 grid sm:flex gap-2 justify-center">
+                                    <a href="/products" class="py-2 px-3 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-stone-500 text-white hover:bg-stone-600 focus:outline-none transition-all text-sm">
+                                        <svg class="w-3 h-3" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M2.63452 7.50001L13.6345 7.5M8.13452 13V2" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
+                                        </svg>
+                                        Select a product
+                                    </a>
+                                </div>
+                                
+                            </div>
+                            <!-- End Body -->
                         </div>
                     </div>
                 </div>
-                @php
-                    $totalPrice += $item->product->price * $item->amount;
-                @endphp
-            @endforeach
+            @else
+                @foreach ($carts as $item)
+                    <div class="max-w-screen-lg px-4 py-1 sm:px-2 lg:px-2 lg:py-1 mx-auto">
+                        <div class="bg-white rounded-xl shadow-lg mt-7 p-4 sm:p-7 ">
+                            <div class="mb-3 flex flex-wrap">
+                                <section class="flex flex-col items-center justify-center w-[25%] max-md:w-full">
+                                    <div class="flex flex-wrap items-center">
+                                        <img src="{{ asset($item->product->image_path) }}" class="h-40 w-40 rounded-3xl">
+                                    </div>
+                                </section>
+
+                                <section class="flex flex-col items-stretch w-[40%] ml-5 max-md:w-full">
+                                    <h2 class="mt-2 text-3xl font-bold text-gray-800 text-black text-left">
+                                        {{ $item->product->name }}
+                                    </h2>
+
+                                    <h1 class="text-2xl font-semibold mt-auto mb-3">
+                                        Quantities: <input type="number" class="quantity-input text-center rounded-xl"
+                                            name="cart_items[{{ $item->id }}][amount]" value="{{ $item->amount }}"
+                                            min="0" max="100"> pieces
+                                    </h1>
+                                </section>
+                                <section class="flex flex-col items-stretch w-[20%] ml-auto mt-auto max-md:w-full">
+                                    <span class="item-price text-2xl font-semibold mt-auto mb-3">Price:
+                                        {{ $item->product->price }} Baht</span>
+                                </section>
+                            </div>
+                        </div>
+                    </div>
+                    @php
+                        $totalPrice += $item->product->price * $item->amount;
+                    @endphp
+                @endforeach
+            @endif
+        </div>
         </div>
         <div class="flex flex-row">
             <div
@@ -103,3 +134,34 @@
         }
     </script>
 @endsection
+
+{{-- <!-- Card -->
+<div class="flex flex-col">
+    <div class="-m-1.5 overflow-x-auto">
+        <div class="p-1.5 min-w-full inline-block align-middle">
+
+            <!-- Body -->
+            <div class="max-w-sm w-full min-h-[400px] flex flex-col justify-center mx-auto px-6 py-4">
+
+                <h2 class="mt-5 font-semibold text-gray-800 dark:text-black">
+                    No products?
+                </h2>
+                <p class="mt-2 text-sm text-gray-600 dark:text-gray-400">
+                    Find yourself nice bakery below
+                </p>
+                <div class="mt-5 grid sm:flex gap-2">
+                    <button type="button"
+                        class="py-2 px-3 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-stone-500 text-white hover:bg-stone-600 focus:outline-none transition-all text-sm">
+                        <svg class="w-3 h-3" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                            viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M2.63452 7.50001L13.6345 7.5M8.13452 13V2" stroke="currentColor" stroke-width="2"
+                                stroke-linecap="round" />
+                        </svg>
+                        Select a product
+                    </button>
+                </div>
+            </div>
+            <!-- End Body -->
+        </div>
+    </div>
+</div> --}}
