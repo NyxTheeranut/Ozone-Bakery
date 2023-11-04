36b3ecc2e343dafc39129a3eca23ecb7b0dd96d5
dujrawee <dujrawee.l@ku.th>
2023-11-05 04:36:01 +0700

    ui all products

--- ozone-bakery/resources/views/layouts/products/index.blade.php
+++ ozone-bakery/resources/views/layouts/products/index.blade.php
@@ -1,24 +1,33 @@
 @extends('layouts.main')
 
 @section('content')
-Pickup Date :
-<form method="post" action="{{ route('pickupDate.set') }}">
-    <input type="date" value="{{$pickupDate}}" id="pickupDate" name="pickupDate" onchange="onPickupDateChange()">
+
+<div class="flex flex-col justify-center items-center pt-10">
+    <span class="text-lg mt-2 mb-2 flex-wrap text-stone-500">Please select pick-up date before browsing our products.</span>
+    <div class="flex flex-wrap flex-row mt-2 py-2 px-3 rounded-xl bg-stone-300">
+        <span class="text-xl font-semibold mt-2 mr-5">Pickup Date: </span>
+        <form method="post" action="{{ route('pickupDate.set') }}">
+            <input class="text-center rounded-3xl border border-stone-300 bg-stone-100 hover:bg-white transition-all"
+            style="border-width: 4px;"
+            type="date" value="{{ $pickupDate }}" id="pickupDate" name="pickupDate" onchange="onPickupDateChange()">
+        </form>
+    </div>
+</div>
+
 
     @include('layouts.subviews.products', [
-    'title' => 'Available Products',
-    'products' => $availableProducts,
-    'pickupDate' => $pickupDate
+        'title' => 'Available Products',
+        'products' => $availableProducts,
+        'pickupDate' => $pickupDate,
     ])
     @include('layouts.subviews.products', [
-    'title' => 'All Products',
-    'products' => $allProducts,
-    'pickupDate' => $pickupDate
+        'title' => 'All Products',
+        'products' => $allProducts,
+        'pickupDate' => $pickupDate,
     ])
 
     <script>
-
-        document.addEventListener('DOMContentLoaded', function () {
+        document.addEventListener('DOMContentLoaded', function() {
             const now = new Date().toISOString().slice(0, 10);
             document.getElementById("pickupDate").setAttribute('min', now);
         });
@@ -39,5 +48,4 @@ Pickup Date :
             })
         }
     </script>
-
-    @endsection
+@endsection
