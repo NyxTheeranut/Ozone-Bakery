85e35db5cb686c09b1521eced6e7aefd8096f581
Nyx <theeranut.s@ku.th>
2023-11-07 09:28:01 +0700

    fix admin navbar

--- ozone-bakery/resources/views/layouts/admin/navbar.blade.php
+++ ozone-bakery/resources/views/layouts/admin/navbar.blade.php
@@ -1,9 +1,9 @@
 <nav class="bg-white border-gray-200 py-2.5">
     <div class="flex flex-wrap items-center justify-between max-w-screen-3xl mx-auto ">
-        <a href="/" class="flex items-center ml-5">
+        <h href="/" class="flex items-center ml-5">
             <img src="https://icons.veryicon.com/png/o/food--drinks/sweet-dessert-icon/croissant-18.png" class="h-6 mr-3 sm:h-9" alt="Logo">
             <span class="self-center text-1xl font-semibold whitespace-nowrap">Ozone Bakery</span>
-        </a>
+        </h>
         <div class="flex items-center lg:order-2">
 
             <div class="flex items-center">
